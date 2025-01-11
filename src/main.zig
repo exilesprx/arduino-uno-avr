6c1f1c0d292f5b4646356bac071aa810d364ce25
Exiles.prx <exiles.prx@gmail.com>
2025-01-11 18:43:49 -0500

    Delay update
    Adding a "helper" function to delay the light by milliseconds. Since the
    uno's max integer value is u16, the delay function leverages a loop to
    delay up to max u16 milliseconds. This is just a quick example for
    testing, so no need to adjust any further.

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,18 @@ const microzig = @import("microzig");
 const port = microzig.chip.peripherals.PORTB;
 
 pub fn main() !void {
-    port.DDRB |= (1 << 5);
-    port.PORTB |= 0x00;
+    port.DDRB |= (1 << 5); // set to output
+    port.PORTB |= 0x00; // turn light off
 
     while (true) {
-        microzig.core.experimental.debug.busy_sleep(1_000);
+        delay(2_000);
         port.PINB |= (1 << 5);
+    }
+}
+
+/// delay - busy wait for a number of milliseconds
+fn delay(ms: u16) void {
+    for (0..ms) |_| {
+        microzig.core.experimental.debug.busy_sleep(1_000);
     }
 }
